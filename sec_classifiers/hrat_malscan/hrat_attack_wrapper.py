339c1f8d712bf738a5200c689ebb12158500d79c
dli <lideqiang@njust.edu.cn>
2023-08-12 22:53:48 +0800

    update

--- sec_classifiers/hrat_malscan/hrat_attack_wrapper.py
+++ sec_classifiers/hrat_malscan/hrat_attack_wrapper.py
@@ -17,7 +17,7 @@ import torch
 from sec_classifiers.hrat_malscan.hrat_malscan_det import MalScan
 from tools import utils
 from sec_classifiers.hrat_malscan.Utils import trans2triple_rw, trans2triple
-import myenv_withconstraints_dli
+from sec_classifiers.hrat_malscan import myenv_withconstraints_dli
 from sec_classifiers.hrat_malscan.model import DQN
 from sec_classifiers.dataset import Dataset
 
