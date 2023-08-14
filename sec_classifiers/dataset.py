09401fee9f09bb4a826867360731861ecae12afc
dli <lideqiang@njust.edu.cn>
2023-08-14 23:58:48 +0800

    update

--- sec_classifiers/dataset.py
+++ sec_classifiers/dataset.py
@@ -67,8 +67,7 @@ class Dataset(torch.utils.data.Dataset):
         data_params = {
             'batch_size': self.batch_size,
             'shuffle': False,
-            'num_workers': 6,
-            'pin_memory': True
+            'num_workers': 6
         }
         assert isinstance(data, (np.ndarray, torch.Tensor))
         if label is None:
