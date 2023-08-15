9049feb475db74e13d59556c9e559421640b5d23
dli <lideqiang@njust.edu.cn>
2023-08-15 10:05:17 +0800

    update

--- sec_classifiers/hrat_malscan/hrat_malscan_det.py
+++ sec_classifiers/hrat_malscan/hrat_malscan_det.py
@@ -42,8 +42,10 @@ class MalScan(BasicClassifier):
         if top_k > 1:
             ind = torch.argsort(dist)
             label = self.train_y[ind[:top_k]]
+            warnings.warn("Do not actually implement.\n")
         else:
             min_value = torch.min(dist)
+            print((dist == min_value).nonzero().squeeze())
             label = self.train_y[(dist == min_value).nonzero().squeeze()]
 
         print(label)
