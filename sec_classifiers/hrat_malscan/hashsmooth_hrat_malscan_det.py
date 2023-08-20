f0c0b0918394b75bd71ec7149d644347b08d831e
dli <lideqiang@njust.edu.cn>
2023-08-20 10:17:34 +0800

    update

--- sec_classifiers/hrat_malscan/hashsmooth_hrat_malscan_det.py
+++ sec_classifiers/hrat_malscan/hashsmooth_hrat_malscan_det.py
@@ -69,7 +69,7 @@ class HashSmooth4MalScan(HashSmooth):
 
             nonzero_idx_sel = self.transform_wrapper(np.tile(nonzero_idx.copy(), (current_batch_size, 1)),
                                                      n_subfeatures).squeeze()
-            print('testtest: ', nonzero_idx_sel)
+            print('testtest: ', nonzero_idx_sel.shape)
             with torch.no_grad():
                 # malscan_features = self.get_extra_feature2(x[:, :2], nonzero_idx_sel, x_sensitive_dix, adj_size, device)
                 pred_batch = self.base_classifier.predict(x[nonzero_idx_sel],
