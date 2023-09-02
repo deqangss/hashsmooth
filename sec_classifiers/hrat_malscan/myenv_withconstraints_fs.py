0d225d9b771522989d17e1a97f3ed25e00eaf7ac
dli <lideqiang@njust.edu.cn>
2023-09-02 13:49:33 +0800

    update

--- sec_classifiers/hrat_malscan/myenv_withconstraints_fs.py
+++ sec_classifiers/hrat_malscan/myenv_withconstraints_fs.py
@@ -570,8 +570,6 @@ class CFGModifierEnvConstraints(object):
                     break
                 feature_tran = self.transformer_obj.transform(torch.tile(feature[None, ...], (current_batch_size, 1)),
                                                               self.transformer_obj.sub_k)
-                # total_time = time.time() - start_time
-                # print("cost time 1-1-1: seconds {:.4}.".format(total_time))
                 dist = torch.cat(
                     [torch.sum((feature_tran[:, None, :] - x[None, ...].to(feature.device)).pow(2), -1) for x in self.X_train])
                 # dist = torch.sum((torch.squeeze(X_train.to(self.device)) - torch.squeeze(feature.float())).pow(2.), 1)
