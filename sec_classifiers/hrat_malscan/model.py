09d301e3d1a8d760f652669590e8af30443aec7d
dli <lideqiang@njust.edu.cn>
2023-09-09 13:24:26 +0800

    update

--- sec_classifiers/hrat_malscan/model.py
+++ sec_classifiers/hrat_malscan/model.py
@@ -83,7 +83,6 @@ class DQN(object):
         with torch.no_grad():
             if np.random.uniform() < EPSILON:
                 action_type = self.eval_net.forward(x)
-                print("pred: ", action_type)
                 action_type = torch.max(action_type, 1)[1].data.cpu().numpy()
             else:  # random
                 action = np.random.randint(actions_num)
