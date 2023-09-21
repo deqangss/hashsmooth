703692eddbee2de55932817a0b9755e1defd1b54
dli <lideqiang@njust.edu.cn>
2023-09-21 14:57:19 +0800

    update

--- sec_classifiers/hrat_malscan/myenv_withconstraints_fs.py
+++ sec_classifiers/hrat_malscan/myenv_withconstraints_fs.py
@@ -567,8 +567,9 @@ class CFGModifierEnvConstraints(object):
                 feature_tran = self.transformer_obj.transform(torch.tile(feature[None, ...], (current_batch_size, 1)),
                                                               self.transformer_obj.sub_k)
 
+                print(self.X_train[0])
                 dist = torch.hstack(
-                    [torch.sum((feature_tran[:, None, :] - x[None, ...].detach().to(feature.device)).pow(2), -1) for x in self.X_train])
+                    [torch.sum((feature_tran[:, None, :] - x[None, ...].detach()).pow(2), -1) for x in self.X_train])
                 # dist = torch.sum((torch.squeeze(X_train.to(self.device)) - torch.squeeze(feature.float())).pow(2.), 1)
                 dist_s, w = _get_nn_sample(dist)
                 loss += torch.sum(w * (torch.sigmoid(self.steep * dist_s)))
