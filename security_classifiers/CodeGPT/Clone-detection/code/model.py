c6c0a1ff18724fcdfaf242c6ef424cc29b632e85
lideqiang <lideqiang@njust.edu.cn>
2025-06-15 23:36:08 +0800

    codegpt smooth2

--- security_classifiers/CodeGPT/Clone-detection/code/model.py
+++ security_classifiers/CodeGPT/Clone-detection/code/model.py
@@ -3,12 +3,11 @@
 import torch
 import torch.nn as nn
 import torch
-from tinycss2 import tokenizer
 from torch.autograd import Variable
 import copy
 import torch.nn.functional as F
-from torch.nn import CrossEntropyLoss, MSELoss
 from torch.utils.data import SequentialSampler, DataLoader
+from torch.nn import CrossEntropyLoss, MSELoss
 import numpy as np
 import math
 
@@ -21,19 +20,17 @@ import torchmalware
 binom_test = np.vectorize(binom_test)
 ABSTAIN = -1
 
-
-
 class RobertaClassificationHead(nn.Module):
     """Head for sentence-level classification tasks."""
 
     def __init__(self, config):
         super().__init__()
         self.dense = nn.Linear(config.hidden_size*2, config.hidden_size)
-        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.dropout = nn.Dropout(config.attn_pdrop)
         self.out_proj = nn.Linear(config.hidden_size, 2)
 
-    def forward(self, features, **kwargs):
-        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
+    def forward(self, x, **kwargs):
+        x = x[:, 0, :]  # take <s> token (equiv. to [CLS])
         x = x.reshape(-1,x.size(-1)*2)
         x = self.dropout(x)
         x = self.dense(x)
@@ -55,9 +52,11 @@ class Model(nn.Module):
         
     def forward(self, input_ids=None,labels=None): 
         input_ids=input_ids.view(-1,self.args.block_size)
-        outputs = self.encoder(input_ids= input_ids,attention_mask=input_ids.ne(self.tokenizer.pad_token_id))[0]
-        logits=self.classifier(outputs)
-        prob=F.softmax(logits)
+        outputs = self.encoder(input_ids= input_ids,attention_mask=input_ids.ne(self.tokenizer.pad_token_id))[0] # 2B * L * D
+        # sequence_lengths = torch.ne(input_ids, self.tokenizer.pad_token_id).sum(-1) - 1
+        # outputs=outputs[range(input_ids.size(0)),sequence_lengths,:] # 2B * D
+        logits=self.classifier(outputs) # 2B * D
+        prob=F.softmax(logits) # B * 2
         if labels is not None:
             loss_fct = CrossEntropyLoss()
             loss = loss_fct(logits, labels)
@@ -65,39 +64,35 @@ class Model(nn.Module):
         else:
             return prob
 
-
     def get_results(self, dataset, batch_size, threshold=0.5):
         '''Given a dataset, return probabilities and labels.'''
         self.query += len(dataset)
         eval_sampler = SequentialSampler(dataset)
-        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=batch_size,num_workers=4,pin_memory=False)
+        eval_dataloader = DataLoader(dataset, sampler=eval_sampler, batch_size=batch_size, num_workers=4,
+                                     pin_memory=False)
 
         ## Evaluate Model
 
         eval_loss = 0.0
         self.eval()
-        logits=[] 
-        labels=[]
+        logits = []
+        labels = []
         for batch in eval_dataloader:
-            inputs = batch[0].to("cuda")       
-            label=batch[1].to("cuda") 
+            inputs = batch[0].to("cuda")
+            label = batch[1].to("cuda")
             with torch.no_grad():
-                lm_loss,logit = self.forward(inputs,label)
-                # 调用这个模型. 重写了反前向传播模型.
+                lm_loss, logit = self.forward(inputs, label)
                 eval_loss += lm_loss.mean().item()
                 logits.append(logit.cpu().numpy())
-                # 和defect detection任务不一样，这个的输出就是softmax值，而非sigmoid值
                 labels.append(label.cpu().numpy())
-        logits=np.concatenate(logits,0)
-        labels=np.concatenate(labels,0)
+        logits = np.concatenate(logits, 0)
+        labels = np.concatenate(labels, 0)
 
         probs = logits
-        pred_labels = [0 if first_softmax  > threshold else 1 for first_softmax in logits[:,0]]
-        # 如果logits中的一个元素，其一个softmax值 > threshold, 则说明其label为0，反之为1
-
+        pred_labels = [0 if first_softmax > threshold else 1 for first_softmax in logits[:, 0]]
         return probs, pred_labels
-
-
+      
+        
 def retain_specific_tokens(inputs_ids_mask, inputs_ids, tokenizer):
     assert inputs_ids_mask.shape == inputs_ids.shape
     boolean_var = torch.zeros_like(inputs_ids, device=inputs_ids.device, dtype=torch.bool)
@@ -212,7 +207,7 @@ class RandomSmooth4LLM(nn.Module, RandomSmooth):
         labels = np.concatenate(labels, 0)
 
         return probs, pred_labels
-
+        
 
 class RandomDelSmooth(object):
     ABSTAIN = ABSTAIN
@@ -398,5 +393,4 @@ class HashSmooth4LLM(nn.Module, HashSmooth):
         labels = np.concatenate(labels, 0)
 
         return probs, pred_labels
-
 
